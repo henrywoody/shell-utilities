7a9384a00ea90da5c088d1b797022130d0c908ce
henrywoody <github@henrywoody.com>
2020-09-20 17:22:13 -0700

    Adds git command for renaming master to main
    Adds a git command (alias) for renaming the master branch to main before
    any commits have been made.

--- git.sh
+++ git.sh
@@ -1,5 +1,6 @@
 alias gitfiles="git diff-tree --no-commit-id --name-only -r"
 alias gitgraph="git log --oneline --decorate --graph --all"
+alias gitrenamemain="git symbolic-ref HEAD refs/heads/main"
 
 
 gitfix() {
