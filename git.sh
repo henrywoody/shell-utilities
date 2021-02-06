e0f5235ca37d300714f1eeb6437fde5c64902831
henrywoody <github@henrywoody.com>
2021-02-06 14:24:37 -0800

    Adds getgitignore function
    Adds the `getgitignore` function, which fetches the stock `.gitignore`
    file for the specified language from
    https://github.com/github/gitignore.

--- git.sh
+++ git.sh
@@ -2,15 +2,18 @@ alias gitfiles="git diff-tree --no-commit-id --name-only -r"
 alias gitgraph="git log --oneline --decorate --graph --all"
 alias gitrenamemain="git symbolic-ref HEAD refs/heads/main"
 
-
 gitfix() {
     commit=$1
     git commit -m "FIX"
     git rebase -i $commit
 }
 
-
 alias cl="clear && ls && if git rev-parse --is-inside-work-tree 2> /dev/null
 then
-  git status
+    git status
 fi"
+
+getgitignore() {
+    language=$1
+    curl https://raw.githubusercontent.com/github/gitignore/master/$language.gitignore
+}
